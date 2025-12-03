375c38f926e3193beed30fe4191ee7c9ae703ea6
HeeJeongSeo <heejung9965@naver.com>
2025-12-03 11:20:44 +0900

    refactor: nav/nonav layouts 추가 및 App.js  구조변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // src/App.jsx
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/NavBar.jsx";
+import DefaultLayout from "./layouts/DefaultLayout.jsx";
+import NoNavLayout from "./layouts/NoNavLayout.jsx";
 
 import LoginPage from "./pages/auth/LoginPage.jsx";
 import MainPage from "./pages/MainPage.jsx";
@@ -10,40 +11,43 @@ import ManagerInquiryPage from "./pages/auth/ManagerInquiryPage.jsx"; // ⭐ 추
 import PopupCreatePage from "./pages/popup/PopupCreatePage.jsx";
 import PopupListPage from "./pages/popup/PopupListPage.jsx";
 import PopupDetailPage from "./pages/popup/PopupDetailPage";
-
+import ChatMainPage from "./pages/chat/ChatMainPage.jsx";
 
 function App() {
   return (
     <div className="min-h-screen bg-secondary-light text-text-black">
-      {/* 항상 NavBar 표시 */}
-      <Navbar />
-
       <Routes>
-        {/* 임시 메인 페이지 */}
-        <Route path="/" element={<MainPage />} />
+        {/* NavBar 사용하는 일반 페이지 */}
+        <Route element={<DefaultLayout />}>
+          {/* 임시 메인 페이지 */}
+          <Route path="/" element={<MainPage />} />
 
-        {/* 로그인 페이지 */}
-        <Route path="/login" element={<LoginPage />} />
+          {/* 로그인 페이지 */}
+          <Route path="/login" element={<LoginPage />} />
 
-        {/* ✅ 네이버 소셜 인증 후 회원가입 페이지 */}
-        <Route path="/signup/social" element={<SignupPage />} />
+          {/* ✅ 네이버 소셜 인증 후 회원가입 페이지 */}
+          <Route path="/signup/social" element={<SignupPage />} />
 
-        {/* 임시 마이페이지 */}
-        <Route path="/mypage" element={<MyPage />} />
+          {/* 임시 마이페이지 */}
+          <Route path="/mypage" element={<MyPage />} />
 
-        {/* ⭐ 매니저 계정 문의 페이지 */}
-        <Route path="/manager-inquiry" element={<ManagerInquiryPage />} />
+          {/* ⭐ 매니저 계정 문의 페이지 */}
+          <Route path="/manager-inquiry" element={<ManagerInquiryPage />} />
 
-        {/* 팝업스토어 등록 페이지 */}
-        <Route path="/popup/register" element={<PopupCreatePage />} />
+          {/* 팝업스토어 등록 페이지 */}
+          <Route path="/popup/register" element={<PopupCreatePage />} />
 
-        {/* 팝업스토어 목록 페이지 */}
-        <Route path="/pop-up" element={<PopupListPage />} />
+          {/* 팝업스토어 목록 페이지 */}
+          <Route path="/pop-up" element={<PopupListPage />} />
 
-        {/* 팝업스토어 상세 페이지 */}
-        <Route path="/popup/:popupId" element={<PopupDetailPage />} />
+          {/* 팝업스토어 상세 페이지 */}
+          <Route path="/popup/:popupId" element={<PopupDetailPage />} />
+        </Route>
 
-
+        {/* NavBar 없는 페이지 (채팅 + 관리자) */}
+        <Route element={<NoNavLayout />}>
+          <Route path="/chat" element={<ChatMainPage />} />
+        </Route>
       </Routes>
     </div>
   );
