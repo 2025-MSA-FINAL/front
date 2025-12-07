9e6caa5171b4b5af2a6354d8e2477780e2d247db
HeeJeongSeo <heejung9965@naver.com>
2025-12-07 21:37:07 +0900

    design: right Column 디자인 수정

--- src/components/chat/middleColumn/PopupRoomSection.jsx
+++ src/components/chat/middleColumn/PopupRoomSection.jsx
@@ -7,23 +7,22 @@ export default function PopupRoomSection() {
   const { popups, fetchPopups, loading, selectedPopup } = useChatPopupStore();
 
   useEffect(() => {
-    if (!selectedPopup) {
-      fetchPopups();
-    }
+    if (!selectedPopup) fetchPopups();
   }, [selectedPopup]);
 
   return (
     <section
       className="
         w-full h-full 
-       bg-primary-soft2/80
-        rounded-[30px] 
+        bg-primary-soft2   /* 플랫 연보라 */
+        rounded-[24px] 
         p-4 
         flex flex-col
-        shadow-card
+        border border-primary-soft
+        shadow-[0_2px_8px_rgba(0,0,0,0.06)]
       "
     >
-      <div className="flex-1 min-h-0 overflow-y-auto scrollbar-hide flex flex-col gap-4">
+      <div className="flex-1 min-h-0 overflow-y-auto scrollbar-hide flex flex-col gap-3">
         {loading ? (
           <LoadingSpinner />
         ) : (
@@ -35,15 +34,6 @@ export default function PopupRoomSection() {
             />
           ))
         )}
-
-        {!loading &&
-          popups.map((popup) => (
-            <PopupRoomItem
-              key={popup.popId}
-              name={popup.popName}
-              popup={popup}
-            />
-          ))}
       </div>
     </section>
   );
